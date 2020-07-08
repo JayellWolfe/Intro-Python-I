ce044f3a212db17ee9c424e78008d66d44a088a6
Jessica Williams <32785700+JayellWolfe@users.noreply.github.com>
2020-07-08 08:08:26 -0400

    Update 11_args.py

--- src/11_args.py
+++ src/11_args.py
@@ -5,7 +5,9 @@
 # the sum. This is what you'd consider to be a regular, normal function.
 
 # YOUR CODE HERE
-
+def f1(x,y):
+    return x + y
+print(f1(1, 2))
 print(f1(1, 2))
 
 # Write a function f2 that takes any number of integer arguments and returns the
@@ -13,7 +15,10 @@ print(f1(1, 2))
 # Note: Google for "python arbitrary arguments" and look for "*args"
 
 # YOUR CODE HERE
-
+def f2(*args):
+    if type(args[0]) == list:
+        return sum(args[0])
+    return sum(args)
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
 print(f2(1, 4, -12))            # Should print -7
@@ -30,7 +35,8 @@ print(f2(a))    # Should print 22
 # Note: Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
-
+def f3(x, y = 1):
+    print(x + y)
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
 
@@ -44,7 +50,12 @@ print(f3(8))     # Should print 9
 # Note: Google "python keyword arguments".
 
 # YOUR CODE HERE
-
+def f4(*args,**kwargs):
+      if len(args) >=1:
+        for key, value in args[0].items():
+            print('key: {}, value: {}'.format(key, value))
+    for key,value in kwargs.items():
+        print('key: {}, value: {}'.format(key,value))
 # Should print
 # key: a, value: 12
 # key: b, value: 30
